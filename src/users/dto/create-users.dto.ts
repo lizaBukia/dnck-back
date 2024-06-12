94e0e4cbf67d39462cb1c3db87eaf9e5ee5bd3f4
nikaj <nikajighauri@gmail.com>
2024-06-12 15:29:02 +0400

    feat: created users module

---
+++ src/users/dto/create-users.dto.ts
@@ -0,0 +1,13 @@
+import { IsNumber, IsString } from 'class-validator';
+
+
+export class CreateUsersDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsNumber()
+  birthYear: number;
+}
---
+++ src/users/interfaces/find-one-user.interface.ts
@@ -0,0 +1,5 @@
+import { UserInterface } from './user.interface';
+
+export interface FindOneUserInterface extends UserInterface {
+  index: number;
+}
