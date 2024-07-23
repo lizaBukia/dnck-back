25a97871fc9c4021398961b36e5bbb6abc8b8f0d
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-07-23 20:10:02 +0400

    feat: add Auth-module

--- src/auth/gurads/gurad.interface.ts
+++ src/auth/gurads/gurad.interface.ts
@@ -1,7 +1,8 @@
-import { SetMetadata, CustomDecorator } from '@nestjs/common';
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
 import { role } from '../enum/user.role';
 
 export const ROLES_KEY: string = 'roles';
 
-export const Roles: (...roles: role[]) => CustomDecorator<string> = (...roles: role[]) =>
-    SetMetadata(ROLES_KEY, roles);
+export const Roles: (...roles: role[]) => CustomDecorator<string> = (
+  ...roles: role[]
+) => SetMetadata(ROLES_KEY, roles);
