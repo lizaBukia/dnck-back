cffe9a01c20f287ea061753409a78f8e7e74fb0a
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-08-12 16:32:35 +0400

    chore: fix import

--- src/statistics/repositorys/statisticks.repository.ts
+++ src/statistics/repositorys/statisticks.repository.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Music } from 'src/musics/entities/musics.entity';
 import { MusicsRepository } from 'src/musics/repositories/musics.repository';
 import { Repository } from 'typeorm';
 import { MusicId } from '../dto/musickId.dto';
 import { Statistic } from '../entity/statistic.entity';
-import { Music } from 'src/musics/entities/musics.entity';
 @Injectable()
 export class StatisticsRepository {
   constructor(
@@ -15,7 +15,7 @@ export class StatisticsRepository {
 
   async addLisendMusic(musickId: MusicId): Promise<Statistic> {
     const { id }: MusicId = musickId;
-    const musics:Music = await this.musicsRepository.findOne(id);
+    const musics: Music = await this.musicsRepository.findOne(id);
     const newMusic: Statistic = new Statistic();
     newMusic.name = musics.name;
     newMusic.imgUrl = musics.imgUrl;
