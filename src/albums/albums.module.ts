c15596d663d56f0aa4085f4beab0db2cb3d4f423
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-08-23 14:39:03 +0400

    chore: fix comments

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { S3Service } from 'src/storage/s3.service';
 import { History } from '../history/entity/history.entity';
+import { HistoryRepository } from '../history/repository/history.repository';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
 import { Album } from './entities/album.entity';
 import { AlbumsRepository } from './repositories/albums.repository';
-import { HistoryRepository } from '../history/repository/history.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Album, History])],
--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { History } from '../history/entity/history.entity';
+import { HistoryRepository } from '../history/repository/history.repository';
 import { S3Service } from './s3.service';
 import { StorageController } from './storage.controller';
-import { HistoryRepository } from '../history/repository/history.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([History])],
