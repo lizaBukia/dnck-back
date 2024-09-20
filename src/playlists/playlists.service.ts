e8e5f82ad2d5b33b75666deb7e0eecc72afabbaa
nikaj <nikajighauri@gmail.com>
2024-09-20 16:41:10 +0400

    chore: fix auth

--- src/playlists/playlists.service.ts
+++ src/playlists/playlists.service.ts
@@ -1,11 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DeleteResult } from 'typeorm';
 import { Music } from '../musics/entities/musics.entity';
+import { SearchQueryDto } from '../search/dto/create-search.dto';
 import { CreatePlaylistDto } from './dto/create-playlist.dto';
 import { UpdatePlaylistDto } from './dto/update-playlist.dto';
 import { Playlist } from './entities/playlist.entity';
 import { PlaylistsRepository } from './repositories/playlists.repository';
-import { SearchQueryDto } from '../search/dto/create-search.dto';
 
 @Injectable()
 export class PlaylistsService {
@@ -18,9 +18,7 @@ export class PlaylistsService {
     return await this.playlistsRepository.create(createPlaylistDto, userId);
   }
 
-  async findAll(
-    searchPLaylistQueryDto: SearchQueryDto,
-  ): Promise<Playlist[]> {
+  async findAll(searchPLaylistQueryDto: SearchQueryDto): Promise<Playlist[]> {
     return await this.playlistsRepository.findAll(
       searchPLaylistQueryDto.search,
     );
