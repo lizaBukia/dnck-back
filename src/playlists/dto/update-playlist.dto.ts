36530f5677b1baad1796a063224881e72db0078c
nikaj <nikajighauri@gmail.com>
2024-09-21 18:04:56 +0400

    refactor: playlists creation

--- src/playlists/dto/update-playlist.dto.ts
+++ src/playlists/dto/update-playlist.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 import { CreatePlaylistDto } from './create-playlist.dto';
 
 export class UpdatePlaylistDto extends PartialType(CreatePlaylistDto) {
   @IsOptional()
+  @IsNumber({}, { each: true })
   musics?: number[];
 }
