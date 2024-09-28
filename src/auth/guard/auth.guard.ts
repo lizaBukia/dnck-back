53a6df8167fa2f5d410bbd2f5d85828797e25249
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-09-28 19:25:40 +0400

    chore: add log

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -53,7 +53,7 @@ export class AuthGuard implements CanActivate {
       request['user'] = payload;
     } catch (err) {
       console.log(err, 'jwt mast be provided');
-      return err
+      return err;
     }
     return true;
   }
