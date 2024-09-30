06d108856dfc21af2d398c543149c9a97b553d3e
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-09-30 20:42:17 +0400

    chore: add logs

--- src/users/dto/change-password.dto.ts
+++ src/users/dto/change-password.dto.ts
@@ -1,6 +1,6 @@
-import { IsString } from 'class-validator';
+import { IsNumberString} from 'class-validator';
 
 export class ChangePasswordDto {
-  @IsString()
+  @IsNumberString()
   password: string;
 }
